9812bc223aeb2d559be9b7d6a0eb4e95b2f37b20
mohan kumar <mkumar3@atlassian.com>
2020-10-07 12:26:02 +0530

    Updates

---
+++ precache-manifest.670b4128937ad5869c86287b712dc2b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8293bdc8595c4029439fab2efa72ca",
+    "url": "/issueView/index.html"
+  },
+  {
+    "revision": "33ecdfc2efefbae7395d",
+    "url": "/issueView/static/css/main.6a529040.chunk.css"
+  },
+  {
+    "revision": "422c4e8e8993bb2f6ec5",
+    "url": "/issueView/static/js/2.38b97dfd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/issueView/static/js/2.38b97dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33ecdfc2efefbae7395d",
+    "url": "/issueView/static/js/main.38af435b.chunk.js"
+  },
+  {
+    "revision": "4a4930f9d7526fb14910",
+    "url": "/issueView/static/js/runtime-main.b06812ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/issueView/static/media/logo.5d5d9eef.svg"
+  }
+]);
